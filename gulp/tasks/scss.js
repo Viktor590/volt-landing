b8f68a59187c93dd4c29932ab584ecd81e76e891
Viktor590 <resident1081994@gmail.com>
2022-04-01 11:33:56 +0300

    start work

---
+++ gulp/tasks/scss.js
@@ -0,0 +1,44 @@
+import dartSass from "sass";
+import gulpSass from "gulp-sass";
+import rename from 'gulp-rename';
+
+import cleanCss from 'gulp-clean-css';
+import autoprefixer from 'gulp-autoprefixer';
+import groupCssMediaQuaries from 'gulp-group-css-media-queries';
+
+const sass = gulpSass(dartSass);
+
+export const scss = () => {
+  return app.gulp.src(app.path.src.scss, { sourcemaps: app.isDev })
+    .pipe(app.plugins.plumber(
+      app.plugins.notify.onError({
+        title: 'SCSS',
+        message: 'Error: <%= error.message %>'
+      })))
+    .pipe(app.plugins.replace(/@img\//g, '../img/'))
+    .pipe(sass({
+      outputStyle: 'expanded'
+    }))
+    .pipe(app.plugins.if(
+      app.isBuild,
+      groupCssMediaQuaries()
+    ))
+    .pipe(app.plugins.if(
+      app.isBuild,
+      autoprefixer({
+        grid: true,
+        overrideBrowserslist: ["last 3 version"],
+        cascade: true
+      })
+    ))
+    // .pipe(app.gulp.dest(app.path.build.css)) //раскомментировать если нужен не сжатый
+    .pipe(app.plugins.if(
+      app.isBuild,
+      cleanCss()
+    ))
+    .pipe(rename({
+      extname: ".min.css"
+    }))
+    .pipe(app.gulp.dest(app.path.build.css))
+    .pipe(app.plugins.browsersync.stream());
+}
